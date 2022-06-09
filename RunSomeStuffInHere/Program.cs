3f908ec73d8a42305013a8883388f35c5e023963
kepa totorica <kepatoto@gmail.com>
2022-06-09 09:12:26 -0700

    adding practice questions

---
+++ RunSomeStuffInHere/Program.cs
@@ -0,0 +1,10 @@
+﻿var l1n3 = new ListNode(5);
+var l1n2 = new ListNode(5/*, l1n3*/);
+var l1 = new ListNode(0, l1n2);
+
+var l2n3 = new ListNode(5);
+var l2n2 = new ListNode(5/*, l2n3*/);
+var l2 = new ListNode(0, l2n2);
+
+Console.WriteLine(new Solution().AddTwoNumbers(l1n3, l2n3));
+
---
+++ RunSomeStuffInHere/RomanNumeralToInt.cs
@@ -0,0 +1,40 @@
+﻿public class RomanNumeralToInt
+{
+    public Dictionary<char, int> numerals = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+
+    public Dictionary<string, int> subtractors = new Dictionary<string, int>()
+        {
+            {"IV", 2},
+            {"IX", 2},
+            {"XL", 20},
+            {"XC", 20},
+            {"CD", 200},
+            {"CM", 200},
+        };
+
+    public int RomanToInt(string romanNumeral)
+    {
+        var count = 0;
+        var previousCharacter = ' ';
+        foreach (var character in romanNumeral)
+        {
+            numerals.TryGetValue(character, out var value);
+            if (subtractors.TryGetValue($"{previousCharacter}{character}", out var specialValue))
+            {
+                count -= specialValue;
+            }
+            count += value;
+            previousCharacter = character;
+        }
+        return count;
+    }
+}
